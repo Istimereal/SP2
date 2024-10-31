6b2f290f4e74364b50bfec177797ae4ca6e3dc2e
Istimereal <jjfapir@gmail.com>
2024-10-31 11:56:46 +0100

    update

---
+++ LibraryRoyaltyCalculator/src/Auther.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Auther {
+
+  private String name;
+  private ArrayList<Title> titles;
+
+  public Auther(String name)
+    {
+        this.name = name;
+        ArrayList<Title> titles = new ArrayList<>();
+    }
+
+    public void addTitle(Title t)
+    {
+        this.titles.add(t);
+    }
+
+public void calculateTotalPay()
+
+}
---
+++ LibraryRoyaltyCalculator/src/LibraryRoyaltyCalculator.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class LibraryRoyaltyCalculator {
+
+
+    public static void main(String[] args) {
+
+
+
+    Auther auther1 = new Auther("Stephen King");
+
+    // ArrayList<Title> titles = new ArrayList<>();
+// PrintedBook("Dark Tower", "Fiction", 20, 600);
+
+    PrintedBook printedBookStephen1 = new PrintedBook
+            ("Dark Tower", "SKØN", 20, 600);
+    PrintedBook printedBookStephen2 = new PrintedBook
+            ("Dark Tower 2", "TE", 15, 75);
+    AudioBook audioBookStephen1 = new AudioBook
+            ("Dark Tower 3", "SKØN", 5, 1020);
+
+    auther1.addTitle(printedBookStephen1);
+    auther1.addTitle(printedBookStephen2);
+    auther1.addTitle(audioBookStephen1);
+
+    }
+}
